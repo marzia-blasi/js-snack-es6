4600d8d0ae2c8828c848164116d74ecd3b50f108
marzia <you@example.com>
2025-06-10 15:37:53 +0200

    add array object

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
 /*Snack 1
-Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore.
-Snack2
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici con peso minore. */
+
+// risultato deve essere bike678
+// in #bike va stampato il risultato
+
+const stable = [
+  {
+    name: "bike123",
+    weight: "300",
+  },
+  {
+    name: "bike456",
+    weight: "200",
+  },
+  {
+    name: "bike678",
+    weight: "150",
+  },
+];
+
+/*Snack2
 Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
-*/
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
