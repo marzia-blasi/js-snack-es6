f6af5e2f945fb7e1fdf62205174b9771ec940019
marzia <you@example.com>
2025-06-10 16:45:40 +0200

    Fix bike weight data type and clean up code comments

--- main.js
+++ main.js
@@ -4,30 +4,44 @@ Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le se
 // risultato deve essere bike678
 // in #bike va stampato il risultato
 
+// Array di bici
+
 const stable = [
   {
     name: "bike123",
-    weight: "300",
+    weight: 300,
   },
   {
     name: "bike456",
-    weight: "200",
+    weight: 200,
   },
   {
     name: "bike678",
-    weight: "150",
+    weight: 150,
   },
 ];
 
+// ciclo per "estrarre" il singolo oggetto dell'array
+let minBike = { weight: 10000 }; //oggetto vuoto
+
 for (let i = 0; i < stable.length; i++) {
   const singleBike = stable[i];
-  console.log(singleBike);
+  // console.log(singleBike);
 
+  //recupero del valore "weight"
   const singleBikeWeight = singleBike["weight"];
 
-  console.log(singleBikeWeight);
-}
+  //console.log(singleBikeWeight);
 
-/*Snack2
-Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
-Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.*/
+  // SE il valore peso è il più basso -> aggiungilo all'elemeno h2 in pagina
+
+  if (singleBikeWeight < minBike["weight"]) {
+    minBike = singleBike;
+  }
+}
+console.log(minBike["name"]);
+
+//.innerText; su h2
+document.getElementById(
+  "bike"
+).innerText = ` La bici con il costo minore è ${minBike["name"]}`;
