646b64fc57953bb64a959bec8a3279efe7742e7c
marzia <you@example.com>
2025-06-10 17:18:46 +0200

    Add initial setup for Snack 2 exercise with football teams array

--- main.js
+++ main.js
@@ -45,3 +45,41 @@ console.log(minBike["name"]);
 document.getElementById(
   "bike"
 ).innerText = ` La bici più leggera è ${minBike["name"]} infatti il suo peso è di ${minBike.weight}`;
+
+/*Snack2
+Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”. Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console. */
+
+// array dove è compilato solo il nome
+
+// generare numeri random per 2 proprietà
+// math random
+// aggiungere i valori
+// championsSq.punti = numerorandom
+// championsSq.falli = numerorandom
+
+// creare un arrary che contenga solo nomi e falli subiti
+// stamparlo in console
+
+const championsSq = [
+  {
+    nome: "napoli",
+    punti: "",
+    falli: "",
+  },
+  {
+    nome: "bologna",
+    punti: "",
+    falli: "",
+  },
+  {
+    nome: "roma",
+    punti: "",
+    falli: "",
+  },
+  {
+    nome: "milano",
+    punti: "",
+    falli: "",
+  },
+];
